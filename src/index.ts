6e6c68cab4e46020bc8e97baa5d628299b0cf297
Giovanna de Freitas <giovannadefreitasaraujo@hotmail.com>
2024-03-07 22:37:41 -0300

    refactore: changing index file and package.json

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
+import { Knex } from './server/database/knex'
 import { server } from './server/server' 
 
-server.listen(process.env.PORT || 3333, () => console.log('app rodando'))
+const startServer = () => {
+    server.listen(process.env.PORT || 3333, () => console.log('app rodando'))
+}
+
+//ao iniciar a aplicação vai rodar as migrations e depois inicializar o servidor
+//verificação com if para ver o ambiente (se estiver no localhost rodamos as migrations de forma manual, e não queremos que sejam executadas o tempo todo)
+if (process.env.IS_LOCALHOST !== 'true') {
+    Knex.migrate.latest().then(() => {
+        startServer()
+    })
+    .catch(console.log)
+} else {
+    startServer()
+}
+
+
+
 
