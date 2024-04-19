a41e5f80afe73cad24a014edb5bfa813e5fc482e
Giovanna de Freitas <giovannadefreitasaraujo@hotmail.com>
2024-04-18 21:39:17 -0300

    feat: add environment config for production

--- src/server/database/knex/index.ts
+++ src/server/database/knex/index.ts
@@ -1,5 +1,14 @@
 import knex from "knex";
+import pg from 'pg'
+//a importação abaixo faz com que as váriaveis de ambiente sejam inicializadas nesse arquivo
+import 'dotenv/config'
 import { development, production, test } from "./Environment"; 
+
+//configuração para o postgres rodar 100% com knex
+if (process.env.NODE_ENV === 'production') {
+    //tratamento de dados
+    pg.types.setTypeParser(20, 'text', parseInt)
+}
 
 const getEnviroment = () => {
     switch (process.env.NODE_ENV) {
@@ -11,4 +20,5 @@ const getEnviroment = () => {
             return development;
     }
 }
+
 export const Knex = knex(getEnviroment())
